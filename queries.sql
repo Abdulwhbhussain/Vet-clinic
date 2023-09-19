7cd4ba867a68d91de482c044693846c99c72e60e
WHB901 <abdulwhbhussain@gmail.com>
2023-09-19 19:29:30 +0500

    Queries Added

--- queries.sql
+++ queries.sql
@@ -47,3 +47,21 @@ from animals;
 COMMIT;
 SELECT *
 FROM animals;
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT *
+FROM animals;
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > DATE '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+COMMIT;
+SELECT *
+FROM animals;
