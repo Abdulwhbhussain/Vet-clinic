fcf01da299ba76e6340803a0f41987118b47e726
WHB901 <abdulwhbhussain@gmail.com>
2023-09-21 21:23:57 +0500

    Data Insertion

--- data.sql
+++ data.sql
@@ -85,3 +85,58 @@ SET owner_id = (
     WHERE full_name = 'Dean Winchester'
   )
 WHERE name IN ('Angemon', 'Boarmon');
+/* Pull Request no.4 */
+INSERT INTO vets(name, age, date_of_graduation)
+VALUES ('William Tatcher', 45, '2000-04-23'),
+  ('Maisy Smith', 26, '2019-01-17'),
+  ('Stephanie Mendez', 64, '1981-05-04'),
+  ('Jack Harkness', 38, '2008-06-08');
+INSERT INTO specialization(vet_id, species_id)
+VALUES(
+    (
+      SELECT id
+      FROM vets
+      WHERE name = 'William Tatcher'
+    ),
+    (
+      SELECT id
+      FROM species
+      WHERE name = 'Pokemon'
+    )
+  ),
+  (
+    (
+      SELECT id
+      FROM vets
+      WHERE name = 'Stephanie Mendez'
+    ),
+    (
+      SELECT id
+      FROM species
+      WHERE name = 'Digimon'
+    )
+  ),
+  (
+    (
+      SELECT id
+      FROM vets
+      WHERE name = 'Stephanie Mendez'
+    ),
+    (
+      SELECT id
+      from species
+      WHERE name = 'Pokemon'
+    )
+  ),
+  (
+    (
+      SELECT id
+      FROM vets
+      WHERE name = 'Jack Harkness'
+    ),
+    (
+      SELECT id
+      FROM species
+      WHERE name = 'Digimon'
+    )
+  );
