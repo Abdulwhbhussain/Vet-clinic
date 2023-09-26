e1e9546d974dbe20bac6cb874edea69e9467889e
WHB901 <abdulwhbhussain@gmail.com>
2023-09-27 00:18:33 +0500

    Tables Created and Relations created

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -34,6 +34,3 @@ CREATE TABLE treatments_histories(
   history_id INT REFERENCES medical_histories(id),
   treatment_id INT REFERENCES treatments(id)
 );
-CREATE INDEX idx_animal_id ON visits(animal_id);
-CREATE INDEX idx_vet_id ON visits (vet_id);
-CREATE INDEX idx_email ON owners(email);
