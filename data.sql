fcd1a352a6595fd4925c813142334e9d6084133f
WHB901 <abdulwhbhussain@gmail.com>
2023-09-26 16:09:32 +0500

    Add Data

--- data.sql
+++ data.sql
@@ -401,3 +401,18 @@ VALUES (
     ),
     '2019-09-29'
   );
+/* Pull Request no.5 */
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+SELECT *
+FROM (
+    SELECT id
+    FROM animals
+  ) animal_ids,
+  (
+    SELECT id
+    FROM vets
+  ) vets_ids,
+  generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+insert into owners (full_name, email)
+select 'Owner ' || generate_series(1, 2500000),
+  'owner_' || generate_series(1, 2500000) || '@mail.com';
